dbdb4c8f8ce19892d56a9a5827420536cb118d7a
Anjanay Raina <anjanayraina326@gmail.com>
2022-09-23 09:02:05 +0530

    Token Working Solution Added

---
+++ Token.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract Token {
+
+  mapping(address => uint) balances;
+  uint public totalSupply;
+
+  constructor(uint _initialSupply) public {
+    balances[msg.sender] = totalSupply = _initialSupply;
+  }
+
+  function transfer(address _to, uint _value) public returns (bool) {
+    require(balances[msg.sender] - _value >= 0);
+    balances[msg.sender] -= _value;
+    balances[_to] += _value;
+    return true;
+  }
+
+  function balanceOf(address _owner) public view returns (uint balance) {
+    return balances[_owner];
+  }
+}
+
+// it solutoin is simple , give the transfer function more token than you have to deduct ,  it will deduct from 
+// your account and since it is an uint , it will underflow and give an ascess amount of tokens
